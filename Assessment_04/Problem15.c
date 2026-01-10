53a744beaa86e3761e4a9957131d144b7191e71d
Gnaneshkanthan15 <gokulgnanesh@gmail.com>
2026-01-10 19:22:51 +0530

    Create Problem15.c

---
+++ Assessment_04/Problem15.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n,first,temp,res,d;
+    d= 1;
+    printf("Enter an number : ");
+    scanf("%d",&n);
+    temp = n;
+    Loop:
+    if(n>=10){
+        n = n/10;
+        d = d*10;
+        goto Loop;
+    }
+    first = n;
+    if(first%2 != 0)
+    {
+        res = temp - 1;
+    }
+    printf("%d",res);
+}

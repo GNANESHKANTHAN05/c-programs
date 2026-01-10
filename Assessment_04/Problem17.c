d16d965f929dd521227417dbea0bec29ff4b20be
Gnaneshkanthan15 <gokulgnanesh@gmail.com>
2026-01-10 19:46:27 +0530

    Create Problem17.c

---
+++ Assessment_04/Problem17.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num,d;
+    d=2;
+    printf("Enter an number : ");
+    scanf("%d",&num);
+    Loop:if(d<num){
+        if(num%d == 0)
+        {
+            printf("Not Prime");
+            goto last;
+        }
+        d++;
+        goto Loop;
+    }
+    printf("Prime");
+    last:return 0;
+}

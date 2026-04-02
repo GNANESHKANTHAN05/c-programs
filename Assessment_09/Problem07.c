3da7a624b41adbf78d605b42fe4917205584b23c
Gnaneshkanthan15 <gokulgnanesh@gmail.com>
2026-04-02 20:33:02 +0530

    Add files via upload

---
+++ Assessment_09/Problem07.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+int main()
+{
+    int a[5],i,j,tmp;
+    printf("Enter 5 numbers:\n");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<5;i++){
+        for(j=i+1;j<5;j++){
+            if(a[i]<a[j])
+            {
+                tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+        }
+    }
+    printf("%d\n");
+    for(i=0;i<5;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+}
